f358c013819b8f51391d8b118ad77f1dc0bc593d
rockethelll <rockethell@hotmail.com>
2023-06-20 12:03:20 +0200

    add test for home page and navbar click

---
+++ src/tests/Home.test.jsx
@@ -0,0 +1,13 @@
+import { it, describe, expect } from 'vitest'
+import { render } from '@testing-library/react'
+import Home from '../pages/Home.jsx'
+
+describe(' test <Home />', () => {
+  it('Home should mount properly', () => {
+    const wrapper = render(<Home />)
+    expect(wrapper).toBeTruthy()
+
+    const h1 = wrapper.container.querySelector('h1')
+    expect(h1?.textContent).toBe('Hello depuis la Home page')
+  })
+})
