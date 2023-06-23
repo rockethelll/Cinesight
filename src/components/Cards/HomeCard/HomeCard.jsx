6cf75f58404d254b16c209d5438cc2dd6ccec4b6
Thomas <thomas.kanthack@gmail.com>
2023-06-23 10:00:45 +0200

    commit before merging

--- src/components/Cards/HomeCard/HomeCard.jsx
+++ src/components/Cards/HomeCard/HomeCard.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function HomeCard({ data }) {
+  console.log(data);
   return (
     <div className="home_page__wrapper">
-      <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${data.poster_path}`} alt="" />
+      <Link to={`/movie/${data.id}`}>Voir les détails</Link>
+      <img
+        src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${data.poster_path}`}
+        alt={`Affiche du film ${data.title}`}
+      />
     </div>
   );
 }
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery, useQueryClient } from "react-query";
+import axiosClient from "../axiosClient";
+import axios from "axios";
+
+function useMovie(id) {
+  return useQuery({
+    queryKey: ["movie"],
+    queryFn: async () => {
+      const { data } = await axiosClient.get(`/movie/${id}`);
+      return data;
+    },
+  });
+}
+export default function MovieDetails() {
+  const { id } = useParams();
+  const [more, setMore] = useState(false);
+  const queryClient = useQueryClient();
+  const { status, data, error, isFetching } = useMovie(id);
+  let date;
+
+  function handleMore() {
+    setMore(!more);
+  }
+
+  function formatDate() {
+    date = new Date(data.release_date);
+  }
+
+  if (status === "success") {
+    formatDate();
+  }
+  if (status === "loading") {
+    return <p>Loading ...</p>;
+  }
+
+  if (status === "error") {
+    return (
+      <p>
+        Error:
+        {error.message}
+      </p>
+    );
+  }
+  return (
+    <div className="movie_details__wrapper">
+      <div className="movie_details__header">
+        <img
+          src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`}
+          alt=""
+        />
+      </div>
+      <div className="movie_details__body">
+        <div className="movie_details--note--like">
+          <div className="movie_details--note">
+            <img src="../images/tmdb-logo.svg" alt="" />
+            <p>{data.vote_average}/10</p>
+          </div>
+          <img src="../images/hearth.svg" alt="" />
+        </div>
+        <div>
+          <h2>{data.title}</h2>
+          <p>sortie le {date.toLocaleDateString()}</p>
+        </div>
+        <div className="movie_details--overview">
+          <p className={`${more ? "open" : "closed"}`}>{data.overview}</p>
+          <button type="button" onClick={handleMore}>
+            {`${more ? "Voir moins" : "Voir plus"}`}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
