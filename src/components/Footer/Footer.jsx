444bfcd9331089e90a09a23f66ff23986334926e
Thomas <thomas.kanthack@gmail.com>
2023-06-26 09:23:12 +0200

    resolve conflict

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,3 @@
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+  return <div>Footer</div>;
 }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -48,40 +48,22 @@ export default function Searchbar() {
           placeholder="Search movie ..."
         />
         <button type="button">
-          <img
-            src="../images/filter.svg"
-            style={{ visibility: 'hidden' }}
-            height="18px"
-            alt="filter logo"
-          />
+          <img src="../images/filter.svg" style={{ visibility: 'hidden' }} height="18px" alt="filter logo" />
         </button>
-        {inputText !== '' ? (
-          <ul className="list-result__wrapper">
-            {status === 'success'
-              ? data.results.slice(0, 5).map((result) => (
-                <Link
-                  // eslint-disable-next-line react/jsx-no-bind
-                  onClick={removeInputText}
-                  to={`/movie/${result.id}`}
-                  key={result.id}
-                >
-                  {result.poster_path !== null ? (
-                    <img
-                      src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${result.poster_path}`}
-                      alt={`Affiche du film ${result.title}`}
-                    />
-                  ) : (
-                    <img
-                      src="../images/image.png"
-                      alt={`Aucune affiche trouver pour le film ${result.title}`}
-                    />
-                  )}
-                  {result.title}
-                </Link>
-              ))
-              : null}
-          </ul>
-        ) : null}
+        <ul className="list-result__wrapper">
+          {status === 'success'
+            ? data.results.slice(0, 5).map((result) => (
+              // eslint-disable-next-line react/jsx-no-bind
+              <Link onClick={removeInputText} to={`/movie/${result.id}`} key={result.id}>
+                {result.title}
+                &nbsp;
+                (
+                {result.release_date}
+                )
+              </Link>
+            ))
+            : null}
+        </ul>
       </form>
     </div>
   );
