abbf4a7ef9ea8a56001d77f40440367697cb35b0
Thomas <thomas.kanthack@gmail.com>
2023-06-30 13:33:42 +0200

    commit before push

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,11 +13,18 @@ export default function Footer() {
         </div>
         <div>
           <p>À propos de nous</p>
-          <Link target="_blank" to="https://github.com/rockethelll">- Alec</Link>
-          <Link target="_blank" to="https://github.com/TGianella">- Théo</Link>
-          <Link target="_blank" to="https://github.com/1996thomas">- Thomas</Link>
-          <Link target="_blank" to="https://developer.themoviedb.org/docs">Merci à <strong>TMDB</strong> pour leur API</Link>
-          
+          <Link target="_blank" to="https://github.com/rockethelll">
+            - Alec
+          </Link>
+          <Link target="_blank" to="https://github.com/TGianella">
+            - Théo
+          </Link>
+          <Link target="_blank" to="https://github.com/1996thomas">
+            - Thomas
+          </Link>
+          <Link target="_blank" to="https://developer.themoviedb.org/docs">
+            Merci à <strong>TMDB</strong> pour leur API
+          </Link>
         </div>
         <div>
           <img src="../images/logo.svg" alt="" />
