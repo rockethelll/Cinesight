922b50ca14ec65fc5375cf623a158a3015e3ccaa
Thomas <thomas.kanthack@gmail.com>
2023-06-30 11:41:08 +0200

    adding new page for tags fund by id, fix watchlist btn position

--- src/pages/SearchByGenre/SearchByGenre.jsx
+++ src/pages/SearchByGenre/SearchByGenre.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import axiosClient from "../../axiosClient";
 import SearchCard from "../../components/Cards/SearchCard/SearchCard";
+import { Link } from "react-router-dom";
 
 function useMovieByGenre(id) {
   return useQuery(["movie_by_genre", id], async () => {
@@ -13,14 +14,22 @@ export default function SearchByGenre() {
   const { id } = useParams();
   const searchByGenreQuery = useMovieByGenre(id);
   const { status, data, error } = searchByGenreQuery;
-
   return (
     <main>
-      <h3 className="my-3" style={{ fontSize: "2vw" }}>
+      <h3 className="my-3" style={{ fontSize: "clamp(20px, 2vw, 3vw)" }}>
         Résultat de la recherche :
       </h3>
       <div className="auto-grid">
-        {data && data.results.map((movie) => <SearchCard key={movie.id} data={movie} />)}
+        {data && data.results.length > 0 ? (
+          data.results.map((movie) => (
+            <SearchCard key={movie.id} data={movie} />
+          ))
+        ) : (
+          <>
+            <p className="my-1">Aucun genre ne correspond</p>
+            <Link to="/">Retour à l&apos; accueil</Link>
+          </>
+        )}
       </div>
     </main>
   );
